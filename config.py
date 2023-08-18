63060c6dd2a89699bcc8f0db8a4669af1e891001
ahdhgfdghd263771783gdgv <141498833+ahdhgfdghd263771783gdgv@users.noreply.github.com>
2023-08-18 21:29:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-ENVIRONMENT = True
+ENVIRONMENT = False
 
 API_ID = 25981592
 API_HASH = "709f3c9d34d83873d3c7e76cdd75b866"
@@ -17,9 +17,9 @@ if ENVIRONMENT:
     LOGS = int(getenv("LOGS", -1001983402883))
 
 else:
-    BOT_TOKEN = ""   # Fill BOT_TOKEN here ( Get it from @BotFather )
-    API_KEY_S1 = ""
-    API_KEY_S2 = ""
+    BOT_TOKEN = "6693127174:AAFaNGdoRzu3YvNhf5woOApM4TRUXeMumio"   # Fill BOT_TOKEN here ( Get it from @BotFather )
+    API_KEY_S1 = "193b40046b3a8791c832fa2a8301e749"
+    API_KEY_S2 = "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMzMTEwNzcsImlhdCI6MTY5MTc3NTA3NywicmF5IjoiZDE1ZTRiY2NjZWMwOGYxZThmN2ZjNzlhNGVkOWYwOTMiLCJzdWIiOjE3NTM2MTV9.rguh-WyQqIHIBISwMB67s4JiE2h2gsONJFlajKPa5llo-q27UdJtyGvdWkTLswJRq0t0JVAJ7Asdkebrn3iNxbFW4Ti56EvLQUsPuMbA7_xPwTZTyKEOSH-eK-m1PPgXxsC3PfSNao2kqmnm23TIfXdlbpzriHJxTXhjGNdqiwwRBQHSgCGKWqkCsXgQideYcUGuUJdn4ubqZ5MGgaxPFust7UfcbK6OCNsq6lW-C9hNk8zKJ7jUbQ4NGDmvEiwCvY9um4G2ePZOofQfnzxoALthDD4vzk59vBUp1Voi_GoLwecX_Yejdwh7kDYLEZ-pw6JcNKEwCaowCmHWabwLQg"
     GATEWAY_API_KEY = ""
     OWNER_ID = 5836841532
     CHANNEL = "https://t.me/HindustanOtp"
